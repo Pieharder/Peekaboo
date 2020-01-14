ddb4c2937bad09a0d159fd638ad3ccb457a4a7d4
geoff goetz and michelle morin <geoff@>
2020-01-14 09:20:05 -0800

    Change classes to ID. Adjust CSS and JS accordingly.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
+// front-end logic:
+
 $(document).ready(function() {
     $(".clickable").click(function() {
-      $(".walrus-showing").toggle();
-      $(".walrus-hidden").toggle();
+      $("#initially-hidden").toggle();
+      $("#initially-showing").toggle();
     });
   });
